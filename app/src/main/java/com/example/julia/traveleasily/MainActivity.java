821af8d39cc0e19886a93dc6d7bbcc98edddd1da
JuliaHsu <hsu.bd100@gmail.com>
2016-02-28 03:30:08 -0600

    Tours Button can work on home pagegit add .

--- app/src/main/java/com/example/julia/traveleasily/MainActivity.java
+++ app/src/main/java/com/example/julia/traveleasily/MainActivity.java
@@ -1,13 +1,14 @@
 package com.example.julia.traveleasily;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +49,9 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void viewTours(View view){
+        Intent intent = new Intent(this, TourPage.class);
+        startActivity(intent);
     }
 }
